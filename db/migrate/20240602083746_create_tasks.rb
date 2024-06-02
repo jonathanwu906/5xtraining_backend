6e03986f418ccea60b86da6964062473a7527427
jonathanwu906@gmail.com <jonathanwu906@gmail.com>
2024-06-02 16:49:52 +0800

    Step 7: Finish establishing task and user model

---
+++ db/migrate/20240602083746_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :content
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :priority
+      t.string :status
+      t.string :label
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
