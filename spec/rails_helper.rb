602fd47cef3d058d3400d2f7dc4dd44d647749d3
jonathanwu906@gmail.com <jonathanwu906@gmail.com>
2024-06-17 17:45:19 +0800

    Step 12 安裝shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,3 +67,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include ActionView::RecordIdentifier, type: :feature
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
