d4b9780b747e6713c503a6e84b6a333da75e3413
jonathanwu906@gmail.com <jonathanwu906@gmail.com>
2024-06-18 16:40:21 +0800

    Step 15 加入列表頁面標題和狀態查詢功能

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -7,4 +7,10 @@ module TasksHelper # rubocop:disable Style/Documentation
                          [I18n.t('tasks.sort_by_end_time'), 'end_time']
                        ], selected: selected_sort_order)
   end
+
+  def status_filter_options(selected_status)
+    options_for_select([[I18n.t('tasks.all_status'), nil]] + Task.statuses.keys.map do |status|
+                                                               [I18n.t("tasks.attributes.status.#{status}"), status]
+                                                             end, selected: selected_status)
+  end
 end
