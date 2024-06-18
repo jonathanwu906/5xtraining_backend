6f5f7bc09a4e0accf560f878c4d9437e00512da8
jonathanwu906@gmail.com <jonathanwu906@gmail.com>
2024-06-18 17:13:08 +0800

    Step 16 加入以優先順序排序的功能

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,7 +4,8 @@ module TasksHelper # rubocop:disable Style/Documentation
   def sort_options(selected_sort_order)
     options_for_select([
                          [I18n.t('tasks.sort_by_created_at'), 'created_at'],
-                         [I18n.t('tasks.sort_by_end_time'), 'end_time']
+                         [I18n.t('tasks.sort_by_end_time'), 'end_time'],
+                         [I18n.t('tasks.sort_by_priority'), 'priority']
                        ], selected: selected_sort_order)
   end
 
