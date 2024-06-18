97657830b620b89cd4e5b6ac642d0e66ece4f9a1
jonathanwu906@gmail.com <jonathanwu906@gmail.com>
2024-06-18 16:40:56 +0800

    Step 15 建立 search index

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_034741) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_042335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "content"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer "priority"
-    t.integer "status"
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time", null: false
+    t.integer "priority", null: false
+    t.integer "status", null: false
     t.string "label"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
     t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.check_constraint "char_length(content) <= 1000", name: "content_length_check"
+    t.check_constraint "char_length(label::text) <= 30", name: "label_length_check"
+    t.check_constraint "char_length(name::text) <= 255", name: "name_length_check"
   end
 
   create_table "users", force: :cascade do |t|
