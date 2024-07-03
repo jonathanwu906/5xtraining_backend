e41be7241c291be15cb5791c48bed162b1e5e5ce
jonathanwu906@gmail.com <jonathanwu906@gmail.com>
2024-07-03 17:37:19 +0800

    Step 16 重構 feature spec 中的 index 部分

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -36,98 +36,101 @@ RSpec.describe 'Tasks' do
   end
 
   describe '#index' do
-    let!(:tasks) do
-      [
-        travel_to(1.day.from_now) { create(:task) },
-        travel_to(2.days.from_now) { create(:task) },
-        travel_to(3.days.from_now) { create(:task) },
-        create(:task, :pending),
-        create(:task, :in_progress),
-        create(:task, :completed),
-        create(:task, :high_priority),
-        create(:task, :medium_priority),
-        create(:task, :low_priority)
-      ]
-    end
-
-    before do
-      visit tasks_path
-    end
-
-    it 'displays all tasks correctly' do
-      tasks.each do |task|
-        expect(page).to have_css("##{dom_id(task)}", text: task.name)
-      end
-    end
-
     context 'when sorting by created time' do
+      let!(:task_created_first) { travel_to(1.day.from_now) { create(:task) } }
+      let!(:task_created_second) { travel_to(2.days.from_now) { create(:task) } }
+      let!(:task_created_third) { travel_to(3.days.from_now) { create(:task) } }
+
       before do
+        visit tasks_path
         select I18n.t('tasks.sort_options.created_at'), from: 'sort_order'
       end
 
       it 'sorts the first created before the second created' do
-        task1_index = page.body.index(tasks[0].name)
-        task2_index = page.body.index(tasks[1].name)
+        task1_index = page.body.index(task_created_first.name)
+        task2_index = page.body.index(task_created_second.name)
         expect(task1_index).to be < task2_index
       end
 
       it 'sorts the second created before the third created' do
-        task2_index = page.body.index(tasks[1].name)
-        task3_index = page.body.index(tasks[2].name)
+        task2_index = page.body.index(task_created_second.name)
+        task3_index = page.body.index(task_created_third.name)
         expect(task2_index).to be < task3_index
       end
     end
 
     context 'when sorting by end time' do
+      let!(:task_ended_first) { travel_to(1.day.from_now) { create(:task) } }
+      let!(:task_ended_second) { travel_to(2.days.from_now) { create(:task) } }
+      let!(:task_ended_third) { travel_to(3.days.from_now) { create(:task) } }
+
       before do
+        visit tasks_path
         select I18n.t('tasks.sort_options.end_time'), from: 'sort_order'
       end
 
-      it 'sorts the first ending before the second ending' do
-        task1_index = page.body.index(tasks[0].name)
-        task2_index = page.body.index(tasks[1].name)
+      it 'sorts the first ended before the second ended' do
+        task1_index = page.body.index(task_ended_first.name)
+        task2_index = page.body.index(task_ended_second.name)
         expect(task1_index).to be < task2_index
       end
 
-      it 'sorts the second ending before the third ending' do
-        task2_index = page.body.index(tasks[1].name)
-        task3_index = page.body.index(tasks[2].name)
+      it 'sorts the second ended before the third ended' do
+        task2_index = page.body.index(task_ended_second.name)
+        task3_index = page.body.index(task_ended_third.name)
         expect(task2_index).to be < task3_index
       end
     end
 
     context 'when sorting by priority' do
+      let!(:task_high_priority) { create(:task, :high_priority) }
+      let!(:task_medium_priority) { create(:task, :medium_priority) }
+      let!(:task_low_priority) { create(:task, :low_priority) }
+
       before do
+        visit tasks_path
         select I18n.t('tasks.sort_options.priority'), from: 'sort_order'
       end
 
       it 'sorts high priority before medium priority' do
-        high_priority_index = page.body.index(tasks[6].name)
-        medium_priority_index = page.body.index(tasks[7].name)
+        high_priority_index = page.body.index(task_high_priority.name)
+        medium_priority_index = page.body.index(task_medium_priority.name)
         expect(high_priority_index).to be < medium_priority_index
       end
 
       it 'sorts medium priority before low priority' do
-        medium_priority_index = page.body.index(tasks[7].name)
-        low_priority_index = page.body.index(tasks[8].name)
+        medium_priority_index = page.body.index(task_medium_priority.name)
+        low_priority_index = page.body.index(task_low_priority.name)
         expect(medium_priority_index).to be < low_priority_index
       end
     end
 
-    context 'when searching by name' do
+    context 'when searching by name', :js do
+      let!(:target_task) { create(:task) }
+      let!(:other_task) { create(:task) }
+
       before do
-        fill_in I18n.t('tasks.search_by_name'), with: tasks[0].name
+        visit tasks_path
+        fill_in I18n.t('tasks.search_by_name'), with: target_task.name
       end
 
-      it { is_expected.to have_css("##{dom_id(tasks[0])}", text: tasks[0].name) }
+      it { is_expected.to have_css("##{dom_id(target_task)}", text: target_task.name) }
+      it { is_expected.to have_no_css("##{dom_id(other_task)}", text: other_task.name) }
     end
 
-    context 'when filtering by status' do
+    context 'when filtering by status', :js do
+      let!(:task_pending) { create(:task, :pending) }
+      let!(:task_in_progress) { create(:task, :in_progress) }
+      let!(:task_completed) { create(:task, :completed) }
+
       before do
+        visit tasks_path
         select I18n.t('tasks.attributes.status.in_progress'), from: 'status'
       end
 
-      it { is_expected.to have_css("##{dom_id(tasks[4])}", text: tasks[4].name) }
+      it { is_expected.to have_css("##{dom_id(task_in_progress)}", text: task_in_progress.name) }
+      it { is_expected.to have_no_css("##{dom_id(task_pending)}", text: task_pending.name) }
+      it { is_expected.to have_no_css("##{dom_id(task_completed)}", text: task_completed.name) }
     end
   end
 
