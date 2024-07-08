a2207612403767660ab152e76ff9e962fd298302
Jonathan Wu <jonathanwu906@gmail.com>
2024-07-08 11:34:59 +0800

    資料庫改動

--- db/schema.rb
+++ db/schema.rb
@@ -1,31 +1,24 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_034741) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_042335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "content"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer "priority"
-    t.integer "status"
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time", null: false
+    t.integer "priority", null: false
+    t.integer "status", null: false
     t.string "label"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
     t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.check_constraint "char_length(content) <= 1000", name: "content_length_check"
+    t.check_constraint "char_length(label::text) <= 30", name: "label_length_check"
+    t.check_constraint "char_length(name::text) <= 255", name: "name_length_check"
   end
 
   create_table "users", force: :cascade do |t|
